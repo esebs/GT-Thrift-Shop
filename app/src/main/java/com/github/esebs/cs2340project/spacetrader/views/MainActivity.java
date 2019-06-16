20ea2b126267736ebc0592f66fe37503e0467839
esebs <sebastian.escobar@gatech.edu>
2019-06-16 19:41:53 -0400

    Added Start screen

--- app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/MainActivity.java
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.github.esebs.cs2340project.spacetrader.views.ConfigurationActivity;
 import com.github.esebs.cs2340project.spacetrader.R;
 
 
