c352532030418c0386eee181f25e446825cae6b6
parkerbdav <parkerbdar@gmail.com>
2019-06-21 14:47:59 -0400

    added Government enum with generator and added Corrupt Police as a police presence option

--- app/src/main/java/com/github/esebs/cs2340project/spacetrader/entities/PolicePresence.java
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/entities/PolicePresence.java
@@ -8,11 +8,12 @@ public enum PolicePresence {
     LIGHT_POLICE,
     STANDARD_POLICE,
     HEAVY_POLICE,
-    POLICE_STATE;
+    POLICE_STATE,
+    CORRUPT_POLICE;
 
     public static PolicePresence generatePolicePresence() {
         Random random = new Random();
-        int key = random.nextInt(9);
+        int key = random.nextInt(10);
         switch (key) {
             case 0: return PolicePresence.NO_POLICE;
             case 1:
@@ -20,6 +21,7 @@ public enum PolicePresence {
             case 6:
             case 7: return PolicePresence.HEAVY_POLICE;
             case 8: return PolicePresence.POLICE_STATE;
+            case 9: return PolicePresence.CORRUPT_POLICE;
             default: return PolicePresence.STANDARD_POLICE;
         }
     }
