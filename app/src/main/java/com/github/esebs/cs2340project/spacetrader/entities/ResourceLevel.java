761e9dad4018ede5485cf869dbd63815d0cfbc8d
parkerbdav <parkerbdar@gmail.com>
2019-06-27 16:20:17 -0400

    Price calculation framework in place

---
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/entities/ResourceLevel.java
@@ -0,0 +1,51 @@
+package com.github.esebs.cs2340project.spacetrader.entities;
+import java.util.Random;
+
+public enum ResourceLevel {
+    NO_SPECIAL_RESOURCES,
+    MINERAL_RICH,
+    MINERAL_POOR,
+    DESERT,
+    LOTS_OF_WATER,
+    RICH_SOIL,
+    POOR_SOIL,
+    RICH_FAUNA,
+    LIFELESS,
+    WEIRD_MUSHROOMS,
+    LOTS_OF_HERBS,
+    ARTISTIC,
+    WARLIKE;
+
+    public static ResourceLevel generateResources() {
+        Random random = new Random();
+        int key = random.nextInt(20);
+        switch (key) {
+            case 1: return ResourceLevel.MINERAL_RICH;
+
+            case 2: return ResourceLevel.MINERAL_POOR;
+
+            case 3: return ResourceLevel.DESERT;
+
+            case 4: return ResourceLevel.LOTS_OF_WATER;
+
+            case 5: return ResourceLevel.RICH_SOIL;
+
+            case 6: return ResourceLevel.POOR_SOIL;
+
+            case 7: return ResourceLevel.RICH_FAUNA;
+
+            case 8: return ResourceLevel.LIFELESS;
+
+            case 9: return ResourceLevel.WEIRD_MUSHROOMS;
+
+            case 10: return ResourceLevel.LOTS_OF_HERBS;
+
+            case 11: return ResourceLevel.ARTISTIC;
+
+            case 12: return ResourceLevel.WARLIKE;
+
+            default: return ResourceLevel.NO_SPECIAL_RESOURCES;
+
+        }
+    }
+}
