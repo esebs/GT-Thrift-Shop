e1337494663969dc783d1d2ae797a56bb1704357
Elio Gerges <egerges3@gatech.edu>
2019-06-29 15:03:44 -0400

    Added MainActivity, SellFragment, SystemFragemnt, BuyFragment, SectionsPageAdapter

---
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/BuyFragment.java
@@ -0,0 +1,21 @@
+package com.github.esebs.cs2340project.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.esebs.cs2340project.spacetrader.R;
+
+public class BuyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.buy_fragment, container, false);
+
+        return view;
+    }
+}
