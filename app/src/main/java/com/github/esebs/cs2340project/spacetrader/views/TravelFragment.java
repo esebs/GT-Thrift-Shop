dece4742a7f774971757a55292c2375908b8a2c9
esebs <sebastian.escobar@gatech.edu>
2019-07-03 17:46:23 -0400

    AndroidX and map

--- app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/TravelFragment.java
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/TravelFragment.java
@@ -1,16 +1,57 @@
 package com.github.esebs.cs2340project.spacetrader.views;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.ButtonBarLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class TravelFragment extends Fragment {
+import com.github.esebs.cs2340project.spacetrader.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class TravelFragment extends MapFragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private MapFragment mapView;
+    private View mView;
+
     /**
-     * OnCreateView is called when the user switches to the 'Travel' tab.
+     * Set up when activity starts
+     * @param savedInstanceState current state
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
+        if (mapView != null) {
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+
+    /**
+     * OnCreateView is called when the user switches to the 'Vehicle' tab.
      *
      * @param inflater
      * @param container
@@ -20,23 +61,28 @@ public class TravelFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        mView = inflater.inflate(R.layout.travel_fragment, container, false);
+
+        return mView;
     }
 
     /**
-     * This method overrides setUserVisibleHint in Fragment.
-     * It will update the tabs next to the current tab
-     * as soon as the user switches to a new tab.
-     *
-     *
-     * @param isVisibleToUser
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
      */
     @Override
-    public void setUserVisibleHint(boolean isVisibleToUser) {
-        super.setUserVisibleHint(isVisibleToUser);
-        if (isVisibleToUser) {
-            getFragmentManager().beginTransaction().detach(this).attach(this).commit();
-        }
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
