a98dea718939d3405e0e0a801dd5e998c24d7ea7
Elio Gerges <egerges3@gatech.edu>
2019-07-06 19:51:05 -0400

    Added all building to map along with event handler for the map markers

--- app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/TravelFragment.java
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/views/TravelFragment.java
@@ -5,31 +5,59 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.github.esebs.cs2340project.spacetrader.R;
+import com.github.esebs.cs2340project.spacetrader.entities.Building;
+import com.github.esebs.cs2340project.spacetrader.model.Model;
+import com.github.esebs.cs2340project.spacetrader.viewmodels.PlayerViewModel;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class TravelFragment extends MapFragment implements OnMapReadyCallback {
+import java.util.List;
 
-    private GoogleMap mMap;
-    private MapFragment mapView;
-    private View mView;
+public class TravelFragment extends Fragment implements OnMapReadyCallback, OnMarkerClickListener {
+
+    private GoogleMap googleMap;
+    private SupportMapFragment mapView;
+    private View view;
+    private Model model = Model.getModelInstance();
+    private PlayerViewModel playerViewModel = new PlayerViewModel();
+
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+
+    private Marker mPerth;
+    private Marker mSydney;
+    private Marker mBrisbane;
+
 
     /**
      * Set up when activity starts
+     *
      * @param savedInstanceState current state
      */
     @Override
@@ -37,16 +65,16 @@ public class TravelFragment extends MapFragment implements OnMapReadyCallback {
         super.onCreate(savedInstanceState);
     }
 
+
+    /**
+     * Sets up the view when the activity starts
+     *
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        mapView = ((MapFragment) getFragmentManager().findFragmentById(R.id.map));
-        if (mapView != null) {
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
     }
 
 
@@ -61,9 +89,18 @@ public class TravelFragment extends MapFragment implements OnMapReadyCallback {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView = inflater.inflate(R.layout.travel_fragment, container, false);
+        view = inflater.inflate(R.layout.travel_fragment, container, false);
 
-        return mView;
+        mapView = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        if (mapView == null) {
+            FragmentManager fm = getFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            mapView = new SupportMapFragment();
+            ft.replace(R.id.map, mapView).commit();
+        }
+        mapView.getMapAsync(this);
+        return view;
     }
 
     /**
@@ -77,12 +114,70 @@ public class TravelFragment extends MapFragment implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        MapsInitializer.initialize(getContext());
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        List<Building> buildings = model.getBuildings();
+
+        for (Building b : buildings) {
+            LatLng currentBuilding = new LatLng(b.getLatitude(), b.getLongitude());
+            Marker m = googleMap.addMarker(new MarkerOptions().position(currentBuilding).title(b.getName()));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentBuilding));
+            m.setTag(0);
+
+        }
+
+        //Set map zoom
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
+
+        //Set a listener for marker click.
+        googleMap.setOnMarkerClickListener(this);
+    }
+
+
+    /**
+     * Called when the user clicks a marker.
+     *
+     * @param marker is the marker pressed on the map
+     */
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getContext());
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount = clickCount + 1;
+            marker.setTag(clickCount);
+
+            alertDialog.setMessage("Would you like to travel to " + marker.getTitle() + " ?")
+                    .setPositiveButton("Travel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //travel
+                            System.out.println("Traveling to " + marker.getTitle());
+
+                            //set new screen
+                            Intent intent = new Intent(getActivity(), MainActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    })
+                    .create()
+                    .show();
+        }
+
+        // Return false to indicate that we have not consumed the event and that we wish
+        // for the default behavior to occur (which is for the camera to move such that the
+        // marker is centered and for the marker's info window to open, if it has one).
+        return false;
     }
 }
+
