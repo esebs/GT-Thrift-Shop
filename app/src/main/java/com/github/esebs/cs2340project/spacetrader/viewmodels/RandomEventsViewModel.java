a7d635768b899a0c927f290907d0d085decd4349
Travis Adams <travistca@gmail.com>
2019-07-18 18:52:44 -0400

    Adds poll method (remove) to RandomEventsViewModel.java

--- app/src/main/java/com/github/esebs/cs2340project/spacetrader/viewmodels/RandomEventsViewModel.java
+++ app/src/main/java/com/github/esebs/cs2340project/spacetrader/viewmodels/RandomEventsViewModel.java
@@ -1,13 +1,12 @@
 package com.github.esebs.cs2340project.spacetrader.viewmodels;
 
-import com.github.esebs.cs2340project.spacetrader.entities.AlbinoSquirrel;
-import com.github.esebs.cs2340project.spacetrader.entities.BobWaters;
 import com.github.esebs.cs2340project.spacetrader.entities.Encounterable;
 import com.github.esebs.cs2340project.spacetrader.entities.Trader;
-//import com.github.esebs.cs2340project.spacetrader.entities.Police;
+import com.github.esebs.cs2340project.spacetrader.entities.Police;
 import com.github.esebs.cs2340project.spacetrader.entities.Pirate;
+import com.github.esebs.cs2340project.spacetrader.entities.AlbinoSquirrel;
+import com.github.esebs.cs2340project.spacetrader.entities.BobWaters;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
@@ -22,7 +21,7 @@ public class RandomEventsViewModel {
     /**
      * Generates a new encounters queue for the ViewModel
      */
-    public void createNewEncounterQueue() {
+    public void createNewEncountersQueue() {
         Queue<Encounterable> newEncounters = new LinkedList<>();
 
         int maxNumEncounters = 5;
@@ -33,31 +32,35 @@ public class RandomEventsViewModel {
             double randomChance = Math.random();
             if (randomChance <= 0.32) {
                 newEncounters.add(new Trader());
-//            } else if (randomChance <= 0.64) {
-//                newEncounters.add(new Police());
+            } else if (randomChance <= 0.64) {
+                newEncounters.add(new Police());
             } else if (randomChance <= 0.96) {
                 newEncounters.add(new Pirate());
             } else if (randomChance <= 0.98) {
                 newEncounters.add(new AlbinoSquirrel());
             } else {
                 newEncounters.add(new BobWaters());
+            }
         }
+        this.encounters = newEncounters;
     }
 
-//    /**
-//     * Gets the encounters during this travel
-//     *
-//     * @return
-//     */
-//    public Queue<Encounterable> getEncounterQueue() {
-//        return encounters;
-//    }
-//
-//    /**
-//     * Sets the encounters during this travel
-//     * @param encounters
-//     */
-//    public void setEncounterQueue(Queue<Encounterable> encounters) {
-//        this.encounters = encounters;
+    /**
+     * Remove and return the next encounter
+     *
+     * @return the head of the encounters queue, if it isn't empty
+     *         (returns null if there are no encounters in the queue)
+     */
+    public Encounterable pollEncountersQueue() {
+        return encounters.poll();
+    }
+
+    /**
+     * Gets the encounters during this travel
+     *
+     * @return
+     */
+    public Queue<Encounterable> getEncountersQueue() {
+        return encounters;
     }
 }
